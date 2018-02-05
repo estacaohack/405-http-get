c560e2e9a11b4f9e335a06b3cba80a6ff22af179
Daniel Toloto <dtoloto@MacBook-Pro-de-Daniel.local>
2018-02-05 10:20:53 -0200

    exemplo http get

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,18 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $http) { //injetar o $http aqui
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $http({
+        method : 'GET',
+        url : 'https://api.themoviedb.org/3/movie/popular?page=01&api_key=327112e6bb7a8217b2c858631a99d106'
+    }).then(function mySuccess(resposta) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+            console.log(resposta.data.results);
+            $scope.filmes = resposta.data.results
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+        }, function myError(resposta) {
+           $scope.erro = resposta.statusText;
+      });
+
 });
